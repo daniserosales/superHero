b3452d294e91c7cac496d303e8b484335351ea1a
laisjmvet <laisjmvet@gmail.com>
2023-07-20 14:07:00 +0100

    compnents working

--- superhero/src/components/SearchWidget/index.jsx
+++ superhero/src/components/SearchWidget/index.jsx
@@ -4,23 +4,21 @@ import { HeroForm, HeroList } from '../';
 export default function SearchWidget () {
 
     const [heroData, setHeroData] = useState([]);
-    const [searchString, setSearchString] = useState("");
-    
-    console.log("rawwwwww")
+    const [searchString, setSearchString] = useState("");    
+   
     useEffect(() => {
 
         async function searchAPI() {
-            console.log("ashgfuihuh")
-            const response = await fetch(`https://www.superheroapi.com/api.php/10223123073014292/search/hulk`);
+            
+            const response = await fetch(`https://www.superheroapi.com/api.php/10223123073014292/search/${searchString}`);
             const rawData = await response.json();
-            console.log(rawData, "ashgfuihuh")
-            const data = rawData.map(s => s.results);
+            const data = rawData.results.map(s => s);
             setHeroData(data);
         }
         
         searchAPI();
         
-    }, []);
+    }, [searchString]);
     
     function handleSearch(userInput){
         setSearchString(userInput);        
